59dacaa12516bd20f3c37d7851877dd88d49fe4f
JSQ180 <112090173+JSQ180@users.noreply.github.com>
2022-09-08 15:51:50 +0000

    🎉 INT COMMIT: 样式修改 Updated By Github Actions

---
+++ js/valine.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".vemoji-btn").text("?"),$("#vcomments").on("click","span.vat",(function(){$(this).parent("div.vmeta").next("div.vcontent").after($("div.vwrap")),$("textarea#veditor").focus()}))}));
